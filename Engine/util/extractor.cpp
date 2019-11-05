d66d5bcac39f718c5fbe3d7e3e5736fbf684fb4f
Ivan Mogilko <ikm_spb@yahoo.com>
2019-11-06 01:02:38 +0300

    HACK: also extract old format dialog texts

--- Engine/util/extractor.cpp
+++ Engine/util/extractor.cpp
@@ -52,6 +52,25 @@ void ExtractGlobalMessages(const GameSetupStruct &game, const String &dest_direc
     delete out;
 }
 
+void ExtractOldDialogs(const std::vector<String> &lines, const String &dest_directory)
+{
+    if (!Path::IsDirectory(dest_directory))
+        return;
+    Stream *out = File::CreateFile(String::FromFormat("%s/olddlg_messages.txt", dest_directory.GetCStr()));
+    if (!out)
+        return;
+    for (size_t i = 0; i < lines.size(); i++)
+    {
+        size_t len = lines[i].GetLength();
+        if (len > 0)
+        {
+            out->Write(lines[i].GetCStr(), len);
+            out->Write("\n", 1);
+        }
+    }
+    delete out;
+}
+
 #define NO_GAME_ID_IN_ROOM_FILE 16325
 
 void ExtractRoomMessages(int from, int to, const GameSetupStruct &game, const String &dest_directory)
--- Engine/util/extractor.h
+++ Engine/util/extractor.h
@@ -1,7 +1,9 @@
+#include <vector>
 #include "util/string.h"
 
 struct GameSetupStruct;
 
 // HACK UTILS
 void ExtractGlobalMessages(const GameSetupStruct &game, const AGS::Common::String &dest_directory);
+void ExtractOldDialogs(const std::vector<AGS::Common::String> &lines, const AGS::Common::String &dest_directory);
 void ExtractRoomMessages(int from, int to, const GameSetupStruct &game, const AGS::Common::String &dest_directory);
