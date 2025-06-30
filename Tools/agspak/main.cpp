92dc6c0dfd008e04eb94321f8bfc4c4cfec9b416
Ivan Mogilko <ikm_spb@yahoo.com>
2025-06-30 21:27:53 +0300

    agspak: merge with agsunpak, make compatible with more commands
    Usage is now:
        agspak <input-dir> <pak-file> [OPTIONS]
            - packs the dir contents into the pak file
        agspak <COMMAND> [<CMD_OPTIONS>]
            - executes an operation with the existing pak file
    
    Supported commands:
        -u, --unpack <pak-file> <out-dir>

--- Tools/agspak/main.cpp
+++ Tools/agspak/main.cpp
@@ -1,43 +1,100 @@
-//-----------------------------------------------------------------------//
+//=============================================================================
+//
+// Adventure Game Studio (AGS)
+//
+// Copyright (C) 1999-2011 Chris Jones and 2011-2025 various contributors
+// The full list of copyright holders can be found in the Copyright.txt
+// file, which is part of this source code distribution.
+//
+// The AGS source code is provided under the Artistic License 2.0.
+// A copy of this license can be found in the file License.txt and at
+// https://opensource.org/license/artistic-2-0/
+//
+//=============================================================================
+// 
+// AGS package file pack/unpack utility.
+// 
 // TODO:
 // * append cmdline option (create new file / append to existing)
-// * option for full recursive subdirs?
-// * option for explicit file list
-//-----------------------------------------------------------------------//
-#include <algorithm>
-#include <stdio.h>
-#include "data/mfl_utils.h"
+// * proper unified error codes for the AGS tools?
+// 
+//=============================================================================
+#include "commands.h"
 #include "data/include_utils.h"
 #include "util/cmdlineopts.h"
-#include "util/file.h"
-#include "util/multifilelib.h"
-#include "util/path.h"
-#include "util/stdio_compat.h"
-#include "util/string_compat.h"
 #include "util/string_utils.h"
 
 using namespace AGS::Common;
-using namespace AGS::Common::CmdLineOpts;
 using namespace AGS::DataUtil;
 
-const char *HELP_STRING = "Usage: agspak <input-dir> <output-pak> [OPTIONS]\n"
-"Options:\n"
-"  -f, --pattern-file <F> set the name F of the file with the include patterns\n"
-"  -p <MB>                split game assets between partitions of this size max\n"
-"  -r                     recursive mode: include all subdirectories too\n"
-"  -v, --verbose          prints packaged files";
+const char *BIN_STRING = "agspak v0.3.0 - AGS game packaging tool\n"
+    "Copyright (c) 2025 AGS Team and contributors\n";
 
-int main(int argc, char *argv[])
+const char *HELP_STRING = "Usage:\n"
+   //--------------------------------------------------------------------------------|
+    "  agspak <input-dir> <pak-file> [OPTIONS]\n"
+    "      - packs the dir contents into the pak file\n"
+    "  agspak <COMMAND> [<CMD_OPTIONS>]\n"
+    "      - executes an operation with the existing pak file\n"
+    "\n"
+    "Packing options:\n"
+    "  -f, --pattern-file <file>\n"
+    "                         use pattern file with the include/exclude patterns\n"
+    "  -p <MB>                split game assets between partitions of this size max\n"
+    "  -r                     recursive mode: include all subdirectories too\n"
+    "\n"
+    "Commands:\n"
+    "  -u, --unpack <pak-file> <out-dir>\n"
+    "                         unpackage all the pak file's contents into the dir\n"
+    "\n"
+//  "Command options:\n"
+//  "\n"
+    "Other options:\n"
+    "  -v, --verbose          print operation details"
+    ;
+
+
+int DoPakCommand(const CmdLineOpts::ParseResult &cmdargs, bool verbose)
 {
-    printf("agspak v0.2.0 - AGS game packaging tool\n"\
-        "Copyright (c) 2024 AGS Team and contributors\n");
-    ParseResult parseResult = Parse(argc,argv,{"-p", "-f", "--pattern-file"});
-    if (parseResult.HelpRequested)
+    // Parse the command
+    // FIXME: CmdLineOpts utility currently do not let us detect whether there was
+    // a '-N' or '--NAME' argument on a specified position;
+    // figure out a better way of handling this (is there?).
+    char command = 0;
+    for (const auto &opt : cmdargs.Opt)
     {
-        printf("%s\n", HELP_STRING);
-        return 0; // display help and bail out
+        if (opt == "-u" || opt == "--unpack")
+        {
+            command = 'u'; // unpack
+            break;
+        }
     }
-    if (parseResult.PosArgs.size() < 2)
+
+    // Run supported commands
+    switch (command)
+    {
+    case 'u': // unpack
+        {
+            const String &src = cmdargs.PosArgs[0];
+            const String &dst = cmdargs.PosArgs[1];
+            if (!src.IsEmpty() && !dst.IsEmpty())
+                return AGSPak::Command_Unpack(src, dst);
+            break; // not enough args
+        }
+    default:
+        printf("Error: command not specified\n");
+        printf("%s\n", HELP_STRING);
+        return -1;
+    }
+
+    printf("Error: not enough arguments\n");
+    printf("%s\n", HELP_STRING);
+    return -1;
+}
+
+int DoPackage(const CmdLineOpts::ParseResult &cmdargs, bool verbose)
+{
+    if (cmdargs.PosArgs.size() < 2)
     {
         printf("Error: not enough arguments\n");
         printf("%s\n", HELP_STRING);
@@ -49,14 +106,13 @@ int main(int argc, char *argv[])
     bool has_include_pattern_file = false;
     String include_pattern_file_name;
 
-    bool verbose = parseResult.Opt.count("-v") || parseResult.Opt.count("--verbose");
-    bool do_subdirs = parseResult.Opt.count("-r");
-    size_t part_size = 0;
-    for (const auto &opt_with_value : parseResult.OptWithValue)
+    const bool do_subdirs = cmdargs.Opt.count("-r");
+    size_t part_size_mb = 0;
+    for (const auto &opt_with_value : cmdargs.OptWithValue)
     {
         if (opt_with_value.first == "-p")
         {
-            part_size = StrUtil::StringToInt(opt_with_value.second);
+            part_size_mb = StrUtil::StringToInt(opt_with_value.second);
         }
         else if (opt_with_value.first == "-f" || opt_with_value.first == "--pattern-file")
         {
@@ -65,83 +121,42 @@ int main(int argc, char *argv[])
         }
     }
 
-    const String &src = parseResult.PosArgs[0];
-    const String &dst = parseResult.PosArgs[1];
-    printf("Input directory: %s\n", src.GetCStr());
-    printf("Output pack file: %s\n", dst.GetCStr());
-    if (has_include_pattern_file)
-        printf("Pattern file name: %s\n", include_pattern_file_name.GetCStr());
+    const String &src = cmdargs.PosArgs[0];
+    const String &dst = cmdargs.PosArgs[1];
+    return AGSPak::Command_Pack(src, dst, include_pattern_file_name, do_subdirs, part_size_mb, verbose);
+}
 
-    if (!ags_directory_exists(src.GetCStr()))
+int main(int argc, char *argv[])
+{
+    printf("%s\n", BIN_STRING);
+
+    CmdLineOpts::ParseResult cmdargs = CmdLineOpts::Parse(argc, argv, {"-p", "-f", "--pattern-file"});
+    if (cmdargs.HelpRequested)
     {
-        printf("Error: not a valid input directory.\n");
+        printf("%s\n", HELP_STRING);
+        return 0; // display help and bail out
+    }
+    if (cmdargs.PosArgs.size() < 1)
+    {
+        printf("Error: not enough arguments\n");
+        printf("%s\n", HELP_STRING);
         return -1;
     }
 
-    //-----------------------------------------------------------------------//
-    // Gather list of files and set up library info
-    //-----------------------------------------------------------------------//
-    const String &asset_dir = src;
-    const String &lib_basefile = dst;
+    // FIXME: CmdLineOpts utility currently do not let us detect whether there was
+    // a '-N' or '--NAME' argument on a specified position; find out whether this is
+    // a limitation of this implementation, or a general convention on parsing args;
+    // figure out a better way of handling this (is there?).
+    const bool is_explicit_command =
+        cmdargs.Opt.count("-u") > 0 || cmdargs.Opt.count("--unpack") > 0;
+    const bool verbose = cmdargs.Opt.count("-v") || cmdargs.Opt.count("--verbose");
 
-    std::vector<String> files;
-    HError err = MakeListOfFiles(files, asset_dir, do_subdirs);
-    if (!err)
+    if (is_explicit_command)
     {
-        printf("Error: failed to gather list of files:\n");
-        printf("%s\n", err->FullMessage().GetCStr());
-        return -1;
+        return DoPakCommand(cmdargs, verbose);
     }
-
-    if (has_include_pattern_file)
+    else
     {
-        std::vector<String> output_files;
-        err = IncludeFiles(files, output_files, asset_dir, include_pattern_file_name, verbose);
-        if (!err)
-        {
-            printf("Error: failed to processes %s file:\n", include_pattern_file_name.GetCStr());
-            printf("%s\n", err->FullMessage().GetCStr());
-            return -1;
-        }
-
-        files = std::move(output_files);
+        return DoPackage(cmdargs, verbose);
     }
-
-    std::vector<AssetInfo> assets;
-    err = MakeAssetListFromFileList(files, assets, asset_dir);
-    if (!err)
-    {
-        printf("Error: failed to prepare list of assets:\n");
-        printf("%s\n", err->FullMessage().GetCStr());
-        return -1;
-    }
-    if (assets.size() == 0)
-    {
-        printf("No valid assets found in the provided directory.\nDone.\n");
-        return 0;
-    }
-
-    AssetLibInfo lib;
-    soff_t part_size_b = part_size * 1024 * 1024; // MB to bytes
-    err = MakeAssetLib(lib, lib_basefile, assets, part_size_b);
-    if (!err)
-    {
-        printf("Error: failed to configure asset library:\n");
-        printf("%s\n", err->FullMessage().GetCStr());
-        return -1;
-    }
-
-    //-----------------------------------------------------------------------//
-    // Write pack file
-    //-----------------------------------------------------------------------//
-    String lib_dir = Path::GetParent(lib_basefile);
-    err = WriteLibrary(lib, asset_dir, lib_dir, MFLUtil::kMFLVersion_MultiV30, verbose);
-    if (!err)
-    {
-        printf("Error: failed to write pack file:\n");
-        printf("%s\n", err->FullMessage().GetCStr());
-        return -1;
-    }
-    printf("Pack file(s) written successfully.\nDone.\n");
-    return 0;
 }
