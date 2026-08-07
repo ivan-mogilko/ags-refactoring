c02d5cbeee7d9b4803f5135ee708ffda47a50126
Ivan Mogilko <ikm_spb@yahoo.com>
2026-08-07 06:21:06 +0300

    AGS.Native, spriteimport: fixed an apparent typo in SortOutPalette

--- Tools/data/sprite_utils.cpp
+++ Tools/data/sprite_utils.cpp
@@ -614,7 +614,7 @@ static void SortOutPalette(BitmapData &bm_data, Palette &pal, const Palette &gam
     }
 
     Palette dst_pal;
-    for (int i = 0; i < 255; ++i) // CHECKME: 255th index is ignored?
+    for (int i = 0; i < PAL_SIZE; ++i)
     {
         if (use_bg_slots) // use either slots
             dst_pal[i] = game_pal[i];
